dfc606212482507f0af4490508a56fbc3768ab79
Maytecalvert <166538878+Maytecalvert@users.noreply.github.com>
2025-05-28 08:00:15 -0300

    clase del martes (desaparecen los objetos al tocarlos)

--- Assets/Mercancias.cs
+++ Assets/Mercancias.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Mercancias : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int scorePoints;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.name == "player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
