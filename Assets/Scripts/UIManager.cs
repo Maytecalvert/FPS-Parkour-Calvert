592b12d7ff575990c1a12752d9d6656589f71a40
Maytecalvert <166538878+Maytecalvert@users.noreply.github.com>
2025-06-06 09:05:07 -0300

    algo raro que funciona

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public DineroManager dineroManager;
+    public Mercancias mercancias;
+    public TextMeshProUGUI Txtprecio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dineroManager = FindObjectOfType<DineroManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dineroManager  && Txtprecio )
+        {
+            Txtprecio.text = "$" + dineroManager.playerMoney.ToString("F2");
+        }
+        else
+        {
+            Debug.Log("hubo un error");
+        }
+    }
+}
